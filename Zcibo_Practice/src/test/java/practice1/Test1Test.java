dba52c69b0251312e7aea532f0223e036a178f0d
LaluPujari <pujarilaluskl@gmail.com>
2023-06-02 17:02:25 +0530

    comitt123

--- Zcibo_Practice/src/test/java/practice1/Test1Test.java
+++ Zcibo_Practice/src/test/java/practice1/Test1Test.java
@@ -1,10 +1,31 @@
 package practice1;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class Test1Test {
+	
+	WebDriver driver;
 	@Test
 	public void m1() {
+		
+		String BROWSER = System.getProperty("Browser");
+		String URL = System.getProperty("url");
+		
+		 
+		 if(BROWSER.equalsIgnoreCase("Chrome")) 
+		 {
+			
+			 driver=new ChromeDriver();
+	 }else
+	 {
+		 driver=new FirefoxDriver();
+	 } 
+		
+		
+		
 		System.out.println("hi");
 	}
 	@Test
